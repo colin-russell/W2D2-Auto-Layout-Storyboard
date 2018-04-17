f2b4e4dad344702557361cbbd7091c1204c92b81
Colin Russell <colinrussell101@gmail.com>
2018-04-17 16:38:00 -0400

    Initial Commit

---
+++ W2D2 Auto Layout Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W2D2 Auto Layout Storyboard
+//
+//  Created by Colin on 2018-04-17.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ W2D2 Auto Layout Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W2D2 Auto Layout Storyboard
+//
+//  Created by Colin on 2018-04-17.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
